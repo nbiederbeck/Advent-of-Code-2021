f45a827d8705f0eb6c0bfd1e9b88fc08c0fa7b93
Noah Biederbeck <noah.biederbeck@tu-dortmund.de>
2021-12-14 17:32:35 +0100

    Dec 6 with Histogram

--- dec06/src/main.rs
+++ dec06/src/main.rs
@@ -1,90 +1,74 @@
-#[derive(Debug, Clone, Copy)]
-struct Lanternfish {
-    age: u8,
-}
-
-impl Lanternfish {
-    fn new() -> Self {
-        Lanternfish { age: 8 }
-    }
-    fn with_age(age: u8) -> Self {
-        Lanternfish { age }
-    }
-}
+const AGES: usize = 9;
 
 /// Fish that stay together for social reasons are shoaling:
 /// https://en.m.wikipedia.org/wiki/Shoaling_and_schooling
 #[derive(Debug, Clone)]
 struct Shoal {
-    fish: Vec<Lanternfish>,
+    fish: [u64; AGES],
 }
 
 impl Shoal {
-    fn new() -> Self {
-        Shoal {
-            fish: Vec::<Lanternfish>::new(),
-        }
-    }
     fn evolve(&mut self) {
-        let mut new_fish = Vec::<Lanternfish>::new();
-
-        for &fish in &self.fish {
-            match fish.age {
-                0 => {
-                    new_fish.push(Lanternfish::new());
-                    new_fish.push(Lanternfish::with_age(6));
-                }
-                _ => new_fish.push(Lanternfish::with_age(fish.age - 1)),
-            }
-        }
-
-        self.fish = new_fish;
+        let new_fish = self.fish[0];
+        self.fish[0] = self.fish[1];
+        self.fish[1] = self.fish[2];
+        self.fish[2] = self.fish[3];
+        self.fish[3] = self.fish[4];
+        self.fish[4] = self.fish[5];
+        self.fish[5] = self.fish[6];
+        self.fish[6] = self.fish[7] + new_fish;
+        self.fish[7] = self.fish[8];
+        self.fish[8] = new_fish;
     }
-    fn evolve_for_days(&mut self, days: u8) {
-        for d in 0..days {
-            println!("{}/{}", d, days);
+    fn evolve_for_days(&mut self, days: usize) {
+        for _ in 0..days {
             self.evolve();
         }
     }
 }
 
 impl From<&str> for Shoal {
-    fn from(s: &str) -> Shoal {
-        let mut shoal = Shoal::new();
+    fn from(s: &str) -> Self {
+        let mut fish = [0; AGES];
 
-        for age in s.split(',').map(str::parse::<u8>) {
+        for age in s.split(',').map(str::parse::<usize>) {
             match age {
                 Ok(a) => {
-                    shoal.fish.push(Lanternfish::with_age(a));
+                    fish[a] += 1;
                 }
-                _ => println!("No More Fish in Shoal."),
+                _ => {},
             }
         }
 
-        shoal
+        Self { fish }
     }
 }
 
 #[cfg(test)]
 mod test {
-    use super::*;
+    use crate::*;
+
     #[test]
-    fn part_one() {
+    fn test() {
         let input = include_str!("input_test");
-        let mut shoal = Shoal::from(input);
-        shoal.evolve_for_days(18);
-        assert_eq!(shoal.fish.len(), 26);
-        shoal.evolve_for_days(80 - 18);
-        assert_eq!(shoal.fish.len(), 5934);
+        assert_eq!(one(input, 18), 26);
+        assert_eq!(one(input, 80), 5934);
+        assert_eq!(two(input), 26984457539);
     }
+}
+
+fn one(input: &str, days: usize) -> u64 {
+    let mut shoal = Shoal::from(input);
+    shoal.evolve_for_days(days);
+    shoal.fish.iter().sum()
+}
+
+fn two(input: &str) -> u64 {
+    one(input, 256)
 }
 
 fn main() {
     let input = include_str!("input");
-    let mut shoal = Shoal::from(input);
-
-    shoal.evolve_for_days(80);
-    println!("Part one: {}", shoal.fish.len());
-
-    println!("Part two is taking time. Will skip.")
+    println!("{}", one(input, 80));
+    println!("{}", two(input));
 }
